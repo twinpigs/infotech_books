5e99cb34d37bb155d3865a9dec7a44e375af194a
martini <mr.owner@gmail.com>
2023-12-03 13:12:38 +0400

    откатился на файловую очередь, отключил кролик в докере: драйвера все-таки сломаны, это все понятно, но в рамках тестового чинить ту мач все-таки.

--- config/rabbit_queue.php
+++ config/rabbit_queue.php
@@ -1,10 +1,7 @@
 <?php
 //Сломан драйвер, вот печаль. Будем работать на файлах, как деды наши!
 return [
-    'class' => \yii\queue\amqp_interop\Queue::class,
-    'port' => 5672,
-    'user' => 'rmuser',
-    'password' => 'rmpassword',
-    'queueName' => 'sms',
-    'driver' => yii\queue\amqp_interop\Queue::ENQUEUE_AMQP_LIB,
+    'class' => yii\queue\amqp_interop\Queue::class,
+    'driver' => yii\queue\amqp_interop\Queue::ENQUEUE_AMQP_BUNNY,
+    'dsn' => 'amqp://rmuser:rmpassword@rabbitmq:5672/%2F',
 ];
